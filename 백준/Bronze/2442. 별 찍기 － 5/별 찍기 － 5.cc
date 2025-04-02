954d448992a055ea444e98a501ec644b728763cd
jaeho Kim <67110069+jaehojaeho1113@users.noreply.github.com>
2025-04-02 19:50:48 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 1;i < N+1;i++) {
+		cout << string(N - i, ' ') << string(2 * i - 1, '*') << endl;
+	}
+}
