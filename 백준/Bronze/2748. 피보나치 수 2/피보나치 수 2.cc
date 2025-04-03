728ba27486f9e60539c7a0cf49f53160dcdcf419
jaeho Kim <67110069+jaehojaeho1113@users.noreply.github.com>
2025-04-03 23:18:55 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long n, a = 0, b = 1, c=1;
+	cin >> n;
+	for (int i = 2;i <= n;i++) {
+		c = a + b;
+		a = b;
+		b = c;
+		
+	}
+	cout << c;
+}
