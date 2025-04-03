e4df52da50cb9b2d68fedb970ddf61389040c4cf
jaeho Kim <67110069+jaehojaeho1113@users.noreply.github.com>
2025-04-03 20:42:06 +0900

    [Bronze V] Title: X보다 작은 수, Time: 260 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, X;
+	cin >> N >> X;
+	int* a = new int[N];
+	for (int i = 0;i < N;i++) {
+		int A;
+		cin >> A;
+		a[i]=A;
+		if (a[i] < X)
+			cout << a[i] << " ";
+	}
+	delete[]a;
+}
